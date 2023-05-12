b52a5b356b4b8b5a205f9b15558550805f1dfaf1
Alumnos <Alumnos@PC207>
2023-05-12 14:21:01 -0300

    Agregar archivos de proyecto.

---
+++ pryApellidoControlesBasicos/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace pryApellidoControlesBasicos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEjeY = new System.Windows.Forms.Label();
+            this.lblEjeX = new System.Windows.Forms.Label();
+            this.lblOculto = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblCopiar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEjeY
+            // 
+            this.lblEjeY.AutoSize = true;
+            this.lblEjeY.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEjeY.Location = new System.Drawing.Point(470, 403);
+            this.lblEjeY.Name = "lblEjeY";
+            this.lblEjeY.Size = new System.Drawing.Size(16, 20);
+            this.lblEjeY.TabIndex = 0;
+            this.lblEjeY.Text = "y";
+            // 
+            // lblEjeX
+            // 
+            this.lblEjeX.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblEjeX.AutoSize = true;
+            this.lblEjeX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEjeX.Location = new System.Drawing.Point(470, 383);
+            this.lblEjeX.Name = "lblEjeX";
+            this.lblEjeX.Size = new System.Drawing.Size(16, 20);
+            this.lblEjeX.TabIndex = 1;
+            this.lblEjeX.Text = "x";
+            this.lblEjeX.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label2_MouseMove);
+            // 
+            // lblOculto
+            // 
+            this.lblOculto.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.lblOculto.AutoSize = true;
+            this.lblOculto.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOculto.Location = new System.Drawing.Point(212, 190);
+            this.lblOculto.Name = "lblOculto";
+            this.lblOculto.Size = new System.Drawing.Size(0, 24);
+            this.lblOculto.TabIndex = 2;
+            this.lblOculto.Visible = false;
+            this.lblOculto.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lblOculto_MouseMove);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(216, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblCopiar
+            // 
+            this.lblCopiar.AutoSize = true;
+            this.lblCopiar.Location = new System.Drawing.Point(177, 390);
+            this.lblCopiar.Name = "lblCopiar";
+            this.lblCopiar.Size = new System.Drawing.Size(35, 13);
+            this.lblCopiar.TabIndex = 4;
+            this.lblCopiar.Text = "label1";
+            this.lblCopiar.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 450);
+            this.Controls.Add(this.lblCopiar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblOculto);
+            this.Controls.Add(this.lblEjeX);
+            this.Controls.Add(this.lblEjeY);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEjeY;
+        private System.Windows.Forms.Label lblEjeX;
+        private System.Windows.Forms.Label lblOculto;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblCopiar;
+    }
+}
+
---
+++ pryApellidoControlesBasicos/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryApellidoControlesBasicos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_MouseMove(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            lblOculto.Text = "Bienvenido";
+            lblOculto.Visible = true;
+                
+   
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            lblEjeX.Text = e.X.ToString();
+            lblEjeY.Text = e.Y.ToString();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblOculto_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            lblCopiar.Text = textBox1.Text;
+        }
+    }
+
+    
+}
